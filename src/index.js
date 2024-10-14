5fd3125c7ee29acddb004c81d50a5f452bb2f0cd
Mahmoud Elzeny <mahmoudelziny20@gmail.com>
2024-10-14 10:41:44 +0300

    ..u

--- src/index.js
+++ src/index.js
@@ -14,13 +14,13 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const router = createBrowserRouter([
   {
-    path: "", element: <App />, children: [
-      { path: "", element: <Home /> },
+    path: "/", element: <App />, children: [
+      { path: "/", element: <Home /> },
       { path: "about", element: <About /> },
       { path: "portfolio", element: <Portfolio /> },
       { path: "contact", element: <Contact /> },
     ]
-  },
+  }
 ])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -28,7 +28,5 @@ root.render(
   <RouterProvider router={router} />
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
